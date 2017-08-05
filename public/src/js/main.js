40ec2ef8b514d10c8f608f7de70352660df7338e
David Moranchel <david.arcshtag@gmail.com>
2017-08-05 17:58:58 -0500

    Connected to firebase

---
+++ public/src/js/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBSyOIp__WrJdnWtAUxUi8W319yUD3ICc0",
+  authDomain: "hack-2018.firebaseapp.com",
+  databaseURL: "https://hack-2018.firebaseio.com",
+  projectId: "hack-2018",
+  storageBucket: "hack-2018.appspot.com",
+  messagingSenderId: "19198448984"
+};
+firebase.initializeApp(config);
